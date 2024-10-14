0c2f291d84d59764985db82923d36fea53b13e1c
anhldptit2610 <anhldptit.2610@gmail.com>
2024-10-14 21:33:59 +0700

    move include/ into core/

--- src/core/mapper/mapper0.cc
+++ src/core/mapper/mapper0.cc
@@ -9,6 +9,7 @@ uint8_t ROM::Mapper0Read(uint16_t addr)
     } else if (IN_RANGE(addr, 0xc000, 0xffff) && header.prgROMSize == 16 * KiB) {
         return prgROM[(addr & 0xbfff) - 0x8000];
     }
+    return 0xff;
 }
 
 void ROM::Mapper0Write(uint16_t addr, uint8_t val)
